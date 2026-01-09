0268c4ed453f12086ea0b648333ec11ae3111b13
Oleksa Panchuk <olexandrpan4uk@gmail.com>
2026-01-09 23:40:54 +0200

    Refactor styles and update SCSS configuration; add theme support and adjust imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'node:path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@use "@/styles/theme" as *;`,
+            },
+        },
+    },
+});

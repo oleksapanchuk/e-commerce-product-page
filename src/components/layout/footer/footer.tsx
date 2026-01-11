7965ad61f0f6c5af96e1020b4abbd3047ef7ad20
Oleksa Panchuk <olexandrpan4uk@gmail.com>
2026-01-11 18:28:18 +0200

    Refactor Footer component styles and structure; add responsive design and link hover effects

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -2,16 +2,25 @@ import styles from './footer.module.scss';
 
 const Footer = () => {
     return (
-        <footer className={styles.container}>
-            <div className='attribution'>
+        <footer className={styles.footerContainer}>
+            <div className={styles.footerContainer__content}>
                 Challenge by{' '}
                 <a
                     href='https://www.frontendmentor.io?ref=challenge'
                     target='_blank'
+                    className={styles.footerContainer__link}
                 >
                     Frontend Mentor
                 </a>
-                . Coded by <a href='#'>Oleksandr Panchuk</a>.
+                . Coded by{' '}
+                <a
+                    href='https://www.frontendmentor.io/profile/oleksapanchuk'
+                    target='_blank'
+                    className={styles.footerContainer__link}
+                >
+                    Oleksandr Panchuk
+                </a>
+                .
             </div>
         </footer>
     );
